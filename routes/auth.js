f1be736d920eec97bd4e5878163c8ebcfd0ee91a
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-07-27 13:06:16 -0400

    built

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const passport = require("passport");
 const router = Router();
 
-router.post("login", passport.authenticate("local"), (req, res) => {
+router.post("/login", passport.authenticate("local"), (req, res) => {
   const params = req.body;
   console.log("/login route hit:", params);
   res.send(res.data);
