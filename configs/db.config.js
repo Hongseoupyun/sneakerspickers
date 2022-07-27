b5dfb8d92134d00f23d7da98edbdbdd04e12bbd0
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-07-27 12:17:35 -0400

    commit

--- configs/db.config.js
+++ configs/db.config.js
@@ -3,19 +3,19 @@ const { Pool } = require('pg');
 
 // const {DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT} = process.env;
 
-const devConfig = {
-	user: process.env.DB_USER,
-	host: process.env.DB_HOST,
-	password: process.env.DB_PASSWORD,
-	port: process.env.DB_PORT,
-	database: process.env.DB_DATABASE,
-};
+// const devConfig = {
+// 	user: process.env.DB_USER,
+// 	host: process.env.DB_HOST,
+// 	password: process.env.DB_PASSWORD,
+// 	port: process.env.DB_PORT,
+// 	database: process.env.DB_DATABASE,
+// };
 
 const proConfig = {
 	connectionString: process.env.DATABASE_URL
 }
 
-const pool = new Pool(process.env.NODE_ENV === "production" ? proConfig : devConfig)
+const pool = new Pool( proConfig )
 
 pool.connect().then(() => {
 	console.log("Database connection established.")
